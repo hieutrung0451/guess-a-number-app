b43426de1b963e498f6cefb39b7d59c16efa6f48
trunghieu <trunghieu0451@gmail.com>
2023-06-11 22:11:47 +0700

    add header

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello world!</Text>
+    <View style={styles.screen}>
+      <Header title='Guess a Number' />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
